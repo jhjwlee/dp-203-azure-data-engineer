c8d591d9ba9fbe23cdbacc55d42aa48a1aed930c
jhjwlee <jh.jw.lee@gmail.com>
2024-12-09 01:46:58 +0900

    Create upload.ps1

---
+++ Allfiles/labs/02/upload.ps1
@@ -0,0 +1,70 @@
+Clear-Host
+Write-Host "Starting script at $(Get-Date)"
+
+# Azure 구독 선택
+$subs = Get-AzSubscription | Select-Object
+if ($subs.GetType().IsArray -and $subs.length -gt 1) {
+    Write-Host "You have multiple Azure subscriptions - please select the one you want to use:"
+    for ($i = 0; $i -lt $subs.length; $i++) {
+        Write-Host "[$($i)]: $($subs[$i].Name) (ID = $($subs[$i].Id))"
+    }
+    $selectedIndex = -1
+    while ($selectedIndex -eq -1) {
+        $enteredValue = Read-Host "Enter a subscription number"
+        if ($enteredValue -match "^\d+$" -and [int]$enteredValue -lt $subs.Length) {
+            $selectedIndex = [int]$enteredValue
+        } else {
+            Write-Host "Invalid selection. Please try again."
+        }
+    }
+    $selectedSub = $subs[$selectedIndex].Id
+    Select-AzSubscription -SubscriptionId $selectedSub
+}
+
+# 사용자 입력 받기
+$resourceGroupName = Read-Host "Enter the Azure resource group name"
+$suffix = Read-Host "Enter the unique suffix for your Azure resources"
+
+# 스토리지 계정 이름 및 컨텍스트 설정
+$dataLakeAccountName = "datalake$suffix"
+
+# 스토리지 계정 확인
+try {
+    $storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $dataLakeAccountName -ErrorAction Stop
+    $storageContext = $storageAccount.Context
+    Write-Host "Storage account found: $dataLakeAccountName in $resourceGroupName"
+} catch {
+    Write-Error "Storage account $dataLakeAccountName not found in resource group $resourceGroupName. Please check the values and try again."
+    Exit
+}
+
+# 파일 업로드
+Write-Host "Uploading files to Azure Storage..."
+
+# CSV 파일 업로드
+Get-ChildItem "./data/*.csv" -File | ForEach-Object {
+    $file = $_.Name
+    $blobPath = "sales/csv/$file"
+    Write-Host "Uploading $file to $blobPath ..."
+    Set-AzStorageBlobContent -File $_.FullName -Container "files" -Blob $blobPath -Context $storageContext
+}
+
+# Parquet 파일 업로드
+Get-ChildItem "./data/*.parquet" -File | ForEach-Object {
+    $file = $_.Name
+    $folder = $_.Name.Replace(".snappy.parquet", "")
+    $newFileName = "orders$($folder).snappy.parquet"
+    $blobPath = "sales/parquet/year=$folder/$newFileName"
+    Write-Host "Uploading $file to $blobPath ..."
+    Set-AzStorageBlobContent -File $_.FullName -Container "files" -Blob $blobPath -Context $storageContext
+}
+
+# JSON 파일 업로드
+Get-ChildItem "./data/*.json" -File | ForEach-Object {
+    $file = $_.Name
+    $blobPath = "sales/json/$file"
+    Write-Host "Uploading $file to $blobPath ..."
+    Set-AzStorageBlobContent -File $_.FullName -Container "files" -Blob $blobPath -Context $storageContext
+}
+
+Write-Host "Script completed at $(Get-Date)"
