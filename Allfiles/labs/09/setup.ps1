b7616ff7dd05283f2c5579efa7f348d6e75b4533
jhjwlee <jh.jw.lee@gmail.com>
2024-12-09 23:16:18 +0900

    Update setup.ps1

--- Allfiles/labs/09/setup.ps1
+++ Allfiles/labs/09/setup.ps1
@@ -135,8 +135,8 @@ $Region = $locations.Get($rand).Location
         }
     }
 }
-Write-Host "Creating $resourceGroupName resource group in $Region ..."
-New-AzResourceGroup -Name $resourceGroupName -Location $Region | Out-Null
+# Write-Host "Creating $resourceGroupName resource group in $Region ..."
+# New-AzResourceGroup -Name $resourceGroupName -Location $Region | Out-Null
 
 # Create Synapse workspace
 $synapseWorkspace = "synapse$suffix"
